8dc373f852cf8d1f26085eb8a7c3c7c0f3980f14
pradhanhitesh <hiteshpradhan@iitgn.ac.in>
2024-01-24 22:00:59 +0530

    added changes

--- scripts/custom.py
+++ scripts/custom.py
@@ -221,7 +221,7 @@ def generate_plot(count_dict,tag_time,format_time):
 
 def generate_html_plot(format_time):
     all_files = os.listdir("./")
-    png_files = [file for file in all_files if file.endswith('.png')]
+    png_files = [file for file in all_files if file.startswith('CountryCount_') and file.endswith('.png')]
 
     template_vars = {
         'plot' : f'<img src="{png_files[0]}" width="750" height="400">',
