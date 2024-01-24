9a1d82d4383569c1cf3fcf14f20be98f55e5fcd2
pradhanhitesh <hiteshpradhan@iitgn.ac.in>
2024-01-24 22:16:48 +0530

    added changes

--- scripts/custom.py
+++ scripts/custom.py
@@ -217,7 +217,7 @@ def generate_plot(count_dict,tag_time,format_time):
                      y = list(count_dict.values()))
     ax.bar_label(ax.containers[0])
     txt = "Last updated on: " + str(format_time) + " IST"
-    plt.text(len(count_dict.keys())/2, max(count_dict.values()), txt, fontsize = 13)
+    plt.text((len(count_dict.keys())/2) + 0.2, max(count_dict.values()), txt, fontsize = 13)
 
     plt.savefig('CountryCount_' + tag_time +'.png',bbox_inches='tight')
     plt.close()
