725474fa9ee5cdaa68c193d8d228ec1d19bc6478
pradhanhitesh <hiteshpradhan@iitgn.ac.in>
2024-01-24 20:07:15 +0530

    added changes

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def main():
 
     custom.update_data()         
     custom.get_metadata(pdf,format_time,tag_time)
-    # custom.send_mail(format_time,tag_time)
+    custom.send_mail(format_time,tag_time)
 
-    # template_vars = custom.generate_html(format_time)
-    # env = Environment(loader=FileSystemLoader("template"))
-    # template = env.get_template("template.html")
-    # output_from_parsed_template = template.render(template_vars)
+    template_vars = custom.generate_html(format_time)
+    env = Environment(loader=FileSystemLoader("template"))
+    template = env.get_template("template.html")
+    output_from_parsed_template = template.render(template_vars)
 
-    # with open("README.md", "w+") as fh:
-    #     fh.write(output_from_parsed_template)
+    with open("README.md", "w+") as fh:
+        fh.write(output_from_parsed_template)
 
 
 if __name__ == "__main__":
